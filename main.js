91a3955e57260559d03f3a8eb906312925b6a878
jack ashton <jackdashton@outlook.com>
2023-05-18 18:22:48 +0100

    Finished

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // select something with data-key 65
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // select something with the class 'key'
   console.log(audio);
   if (!audio) return;
+
+  key.classList.add('playing');
   audio.currentTime = 0; // Rewind to the start to allow play repeatedly.
   audio.play();
-  console.log(key);
-  key.classList.add("playing");
-  // transition end event
-}
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // skip if not a transform
-  this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+const keys = Array.from(document.querySelectorAll('.keys'));
 // Each key gets an event listener added to it, when transition ending we run removeTransition
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener("keydown", playSound);
